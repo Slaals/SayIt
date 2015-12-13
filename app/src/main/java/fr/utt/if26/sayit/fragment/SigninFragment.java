875327f36ee55a769ea50ac0ea52e5746c9aec2c
PabloPrudhommeau <pablo.prudhommeau@utt.fr>
2015-12-13 03:18:47 +0100

    Patch of @0d52ebc6b95d8a02b7f35c6296bb3cd67ddb0de3 : bad SigninFragment content

--- app/src/main/java/fr/utt/if26/sayit/fragment/SigninFragment.java
+++ app/src/main/java/fr/utt/if26/sayit/fragment/SigninFragment.java
@@ -1,21 +1,31 @@
 package fr.utt.if26.sayit.fragment;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import java.util.ArrayList;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import fr.utt.if26.itsaysclient.ApiHttpClient;
+import fr.utt.if26.itsaysclient.ItSaysEndpoints;
 import fr.utt.if26.sayit.R;
-import fr.utt.if26.sayit.adapter.ExpressionListAdapter;
-import fr.utt.if26.sayit.bean.Country;
-import fr.utt.if26.sayit.bean.ExpressionItem;
+import fr.utt.if26.sayit.activity.MainActivity;
+import fr.utt.if26.sayit.utils.SharedPreferencesManager;
 
-public class SigninFragment extends Fragment {
+public class SignInFragment extends Fragment {
+
+    private TextView usernameView;
+    private TextView passwordView;
+    private ProgressBar progressBarView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,37 +33,47 @@ public class SigninFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_expression_list, container, false);
-
-        ArrayList<ExpressionItem> listCity = new ArrayList<>();
-        listCity.add(new ExpressionItem("Bonjour, je suis un ours polaire à l'insu de mon plein grès", Country.FRANCE));
-        listCity.add(new ExpressionItem("C'est ici le pays des coqs ?", Country.FRANCE));
-        listCity.add(new ExpressionItem("Hello, where can I find George Cloney ?", Country.UNITED_STATES));
-        listCity.add(new ExpressionItem("The house is bigger than the cat", Country.UNITED_STATES));
-        listCity.add(new ExpressionItem("Hi brudha, what's up ?!", Country.UNITED_STATES));
-        listCity.add(new ExpressionItem("I'm a scientist, and H..em.. I am wondering whether chickens had teeth ?", Country.UNITED_STATES));
-        listCity.add(new ExpressionItem("Salut, je me présente, bob, ancien cosmonaute de boulevard réduit, j'aimerais savoir quelles sont les différences entre des ours polaire kozaques et des épagneuls bretons ?", Country.FRANCE));
-
-        ListView listView = (ListView) view.findViewById(R.id.expressionListListView);
-        listView.setAdapter(new ExpressionListAdapter(this.getActivity(), R.layout.item_expression_list, listCity));
+        View view = inflater.inflate(R.layout.fragment_signin, container, false);
+        usernameView = (TextView) view.findViewById(R.id.loginUsername);
+        passwordView = (TextView) view.findViewById(R.id.loginPassword);
+        progressBarView = (ProgressBar) view.findViewById(R.id.loginProgressBar);
         return view;
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-    }
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        Button signInButton = (Button) view.findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameView.getText().toString();
+                String password = passwordView.getText().toString();
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-    }
+                progressBarView.setVisibility(View.VISIBLE);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
+                // Call the SignIn endpoint of ItSays API
+                ItSaysEndpoints.UserEndpoint.signin(username, password, new ApiHttpClient.ApiCallFinished() {
+                    @Override
+                    public void onApiCallFinished(JSONObject jsonObject) {
+                        progressBarView.setVisibility(View.GONE);
+                        try {
+                            if (jsonObject.getBoolean("success")) {
+                                SharedPreferences sharedPreferences = getContext().getSharedPreferences(SharedPreferencesManager.USER_PREFERENCES, Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString(SharedPreferencesManager.PERMANENT_TOKEN, jsonObject.getString("token"));
+                                editor.apply();
+                                Intent openMainActivity = new Intent(getContext(), MainActivity.class);
+                                openMainActivity.setFlags(openMainActivity.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                startActivity(openMainActivity);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
